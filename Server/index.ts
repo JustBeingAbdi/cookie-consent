11107eb8d9970beb3ee329e5fffe95a81c40e8d2
Abdi Hassan <73824306+JustBeingAbdi@users.noreply.github.com>
2021-03-16 16:48:01 +0100

    Update index.ts

--- Server/index.ts
+++ Server/index.ts
@@ -28,6 +28,9 @@ export class Server {
         res.render("index", {
             config: Config
         });
+    });
+    app.get("/reset", async(req, res) => {
+        res.render("reset");
     })
     
 
